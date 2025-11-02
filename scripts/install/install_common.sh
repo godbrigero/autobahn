423c72c07bd0009a627de4800744c0ad6e8e21bc
brigerodev <golden.notch.creeper@gmail.com>
2025-11-02 15:09:18 -0800

    update install common to be root

--- scripts/install/install_common.sh
+++ scripts/install/install_common.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
+
+sudo apt-get install -y avahi-daemon avahi-utils libnss-mdns protobuf-compiler
 
 if ! command -v rustup &> /dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    source $HOME/.cargo/env
+    source ~/.cargo/env
 fi
 
 rustup install stable
 rustup default stable
-
-apt-get install -y avahi-daemon avahi-utils libnss-mdns protobuf-compiler
